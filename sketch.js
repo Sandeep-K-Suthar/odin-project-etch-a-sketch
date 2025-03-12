e2a7d31daa4a83ef1ee45f923604cecde1df9b92
Sandeep Suthar <sandeep.khemani@outlook.com>
2025-03-13 02:56:40 +0500

    create random color effect for each div

--- sketch.js
+++ sketch.js
@@ -1,20 +1,34 @@
 
 const flexbox = document.querySelector('.flexbox');
 
-function createDivLogic(){
-    
-    for (let index = 0; index < 16*16; index++) {
+function createDivLogic() {
+
+    for (let index = 0; index < 16 * 16; index++) {
         const createDiv = document.createElement('div');
-         flexbox.appendChild(createDiv);
-         createDiv.innerText=index;
-        //  createDiv.classList.add('hover-effect');
-        //  flexbox.querySelector('div').classList.add('hover-effect');
+        flexbox.appendChild(createDiv);
+        createDiv.innerText = index;
     };
-    
 };
 
 createDivLogic();
 
 
-// flexbox.querySelector('div').style.background='green';
+const divRandomColor = () => {
+    const color = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + color.slice(0, 6);
+}
+
+flexbox.querySelectorAll('.flexbox > div').forEach(div => {
+
+    div.classList.add('hover-effect');
+    div.addEventListener('mouseover', () => {
+        div.classList.add('hovered');
+        div.style.backgroundColor = divRandomColor();
+
+    });
+    div.addEventListener('mouseout', () => {
+        div.classList.remove('hovered')
+        div.style.background = '';
+    });
+});
 
