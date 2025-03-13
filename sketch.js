338abdd71d11eabae2165d55f07a39363399f6a0
Sandeep Suthar <sandeep.khemani@outlook.com>
2025-03-13 07:39:49 +0500

    handle cell index logic

--- sketch.js
+++ sketch.js
@@ -3,10 +3,11 @@ const flexbox = document.querySelector('.flexbox');
 
 function createDivLogic() {
 
-    for (let index = 0; index < 16 * 16; index++) {
+    for (let index = 1; index <= 16 * 16; index++) {
         const createDiv = document.createElement('div');
-        flexbox.appendChild(createDiv);
         createDiv.innerText = index;
+        flexbox.appendChild(createDiv);
+
     };
 };
 
@@ -18,17 +19,31 @@ const divRandomColor = () => {
     return '#' + color.slice(0, 6);
 }
 
-flexbox.querySelectorAll('.flexbox > div').forEach(div => {
+flexbox.querySelectorAll('.flexbox > div').forEach((div, index) => {
 
+    div.style.color = 'rgba(0, 0, 0, 0)';
+    div.style.borderColor = 'black'
     div.classList.add('hover-effect');
+
+    let hoverCount = 0;
+    let indexOpacity = 0;
+
     div.addEventListener('mouseover', () => {
+        hoverCount++;
+        indexOpacity += 0.2;
         div.classList.add('hovered');
         div.style.backgroundColor = divRandomColor();
+        if (hoverCount < 5 && index) {
+            div.style.color = `rgba(0, 0, 0, ${indexOpacity})`;
+        };
 
     });
     div.addEventListener('mouseout', () => {
-        div.classList.remove('hovered')
-        div.style.background = '';
+
+        div.classList.remove('hovered');
+        if (div.style.background != 'black') {
+            div.style.background = '';
+        };
     });
 });
 
